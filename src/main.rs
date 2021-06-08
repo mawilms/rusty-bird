b5b50bf1d04f1b10788fe284cc8f6f8635407b75
Marius Wilms <wilms.marius95@gmail.com>
2021-06-08 18:09:01 +0200

    Basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![warn(clippy::all, clippy::pedantic)]
+use clap::{load_yaml, App};
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 }
