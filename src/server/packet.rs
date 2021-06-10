ed48400b49aae394516af6b40ea618ac671c0f35
Marius Wilms <wilms.marius95@gmail.com>
2021-06-10 09:42:58 +0200

    Renamed a class variable

--- src/server/packet.rs
+++ src/server/packet.rs
@@ -4,15 +4,15 @@ use serde::Serialize;
 pub struct Packet {
     player: (f32, f32),
     score: i32,
-    tubes: Vec<((f32, f32), (f32, f32))>,
+    pipes: Vec<((f32, f32), (f32, f32))>,
 }
 
 impl Packet {
-    pub fn new(player: (f32, f32), score: i32, tubes: Vec<((f32, f32), (f32, f32))>) -> Self {
+    pub fn new(player: (f32, f32), score: i32, pipes: Vec<((f32, f32), (f32, f32))>) -> Self {
         Self {
             player,
             score,
-            tubes,
+            pipes,
         }
     }
 }
